d3f193a6b8092940492dc1e4973ac30ea7effe8d
songlinfeng <1123559142@qq.com>
2019-08-12 13:39:58 +0800

    最后提交

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 // 由于 webpack 是基于Node进行构建的，所有，webpack的配置文件中，任何合法的Node代码都是支持的
-var path = require('path')
+let path = require('path');
 // 在内存中，根据指定的模板页面，生成一份内存中的首页，同时自动把打包好的bundle注入到页面底部
 // 如果要配置插件，需要在导出的对象中，挂载一个 plugins 节点
-var htmlWebpackPlugin = require('html-webpack-plugin')
+let HtmlWebpackPlugin = require('html-webpack-plugin');
 
 // 当以命令行形式运行 webpack 或 webpack-dev-server 的时候，工具会发现，我们并没有提供 要打包 的文件的 入口 和 出口文件，此时，他会检查项目根目录中的配置文件，并读取这个文件，就拿到了导出的这个 配置对象，然后根据这个对象，进行打包构建
 module.exports = {
@@ -12,7 +12,7 @@ module.exports = {
     filename: 'bundle.js' // 指定输出文件的名称
   },
   plugins: [ // 所有webpack  插件的配置节点
-    new htmlWebpackPlugin({
+    new HtmlWebpackPlugin({
       template: path.join(__dirname, './src/index.html'), // 指定模板文件路径
       filename: 'index.html' // 设置生成的内存页面的名称
     })
